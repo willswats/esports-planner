1f9eda3435b14fb71489bcc7fa76ddc5a95548f1
willswats <williamstuwatson@gmail.com>
2023-01-02 22:38:54 +0000

    Modify sort function

--- utils/sortArrayByPropertyAlphabetically.js
+++ utils/sortArrayByPropertyAlphabetically.js
@@ -1,10 +1,11 @@
-// This is a modified version of the answer here: https://stackoverflow.com/a/8900824
 export const sortArrayByPropertyAlphabetically = (array) => {
-  let newArray = array;
-  newArray.sort((a, b) => {
-    let textA = a.name.toUpperCase();
-    let textB = b.name.toUpperCase();
-    return textA < textB ? -1 : textA > textB ? 1 : 0;
+  return array.sort((a, b) => {
+    if (a.name.toUpperCase() < b.name.toUpperCase()) {
+      return -1;
+    }
+    if (a.name.toUpperCase() > b.name.toUpperCase()) {
+      return 1;
+    }
+    return 0;
   });
-  return newArray;
 };
